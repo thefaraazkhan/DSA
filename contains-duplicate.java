25bcb724917689ddd056b21153980540d47414f4
thefaraazkhan <khanmohdfaraaz@gmail.com>
2022-10-05 17:01:46 +0530

    more

---
+++ contains-duplicate.java
@@ -0,0 +1,17 @@
+public class Main
+{
+	
+    public boolean containsDuplicate(int[] nums) {
+      HashSet<Integer> set = new HashSet();
+        for(int i = 0; i < nums.length; i++){
+        if(!set.add(nums[i])){
+            return true;
+        }
+        }
+        return false; 
+    }
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
